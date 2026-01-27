08440482cec68fc71bbc2afd7658f9394fe670d9
hcountry79-hub <hcountry79@gmail.com>
2026-01-27 15:34:46 -0600

    Delete infra directory

--- infra/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project"              { type = string }
-variable "environment"          { type = string  default = "dev" }
-variable "aws_region"           { type = string  default = "us-east-2" }
-
-variable "domain"               { type = string }
-variable "hosted_zone_id"       { type = string }
-
-variable "mapbox_public_token"  { type = string }
-variable "stripe_secret_key"    { type = string  sensitive = true }
