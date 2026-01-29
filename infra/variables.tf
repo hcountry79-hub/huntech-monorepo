61303c74d8735ae22544213181362178c56f563f
hcountry79-hub <hcountry79@gmail.com>
2026-01-29 16:48:54 -0600

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,8 +1,31 @@
-variable "project"               { type = string  default = "huntech" }
-variable "environment"           { type = string  default = "dev" }
-variable "aws_region"            { type = string  default = "us-east-2" }
+variable "project" {
+  type    = string
+  default = "huntech"
+}
 
-variable "domain"                { type = string  default = "" }
-variable "hosted_zone_id"        { type = string  default = "" }
-variable "mapbox_public_token"   { type = string  default = "" }
-variable "stripe_secret_key"     { type = string  default = ""  sensitive = true }
+# For general AWS resources you can change this later.
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "domain" {
+  type    = string
+  default = "huntechusa.com"
+}
+
+variable "hosted_zone_id" {
+  type    = string
+  default = ""
+}
+
+variable "mapbox_public_token" {
+  type    = string
+  default = ""
+}
+
+variable "stripe_secret_key" {
+  type      = string
+  default   = ""
+  sensitive = true
+}
